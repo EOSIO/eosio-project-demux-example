5ea52f09326eb7a0aa9d6800100c89233ef6cabe
TaraTritt <tmt7rc@virginia.edu>
2018-07-06 18:01:59 -0400

    Adding backend folder with express

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+import express from "express"
+
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))

cc458006113be55e85a5e1602c131a406eea4c80
TaraTritt <tmt7rc@virginia.edu>
2018-07-12 15:43:09 -0400

    Updated demux simple example now works

--- backend/demux.js
+++ backend/demux.js
@@ -3,30 +3,30 @@ const {
       eos: { NodeosActionReader } // Let's read from an EOS node
     },
     watchers: { BaseActionWatcher }, // Don't need anything special, so let's use the base Action Watcher
-  } = require("demux-js")
+  } = require("demux")
   
-  // Assuming you've already created a subclass of AbstractActionHandler
-  const MyActionHandler = require("./MyActionHandler")
+// Assuming you've already created a subclass of AbstractActionHandler
+const MyActionHandler = require("./demux/MyActionHandler")
+
+// Import Updaters and Effects, which are arrays of objects:
+// [ { actionType:string, (updater|effect):function }, ... ] 
+const updaters = require("./demux/updaters")
+const effects = require("./demux/effects")
+
+const actionHandler = new MyActionHandler(
+  updaters,
+  effects,
+)
+
+const actionReader = new NodeosActionReader(
+  "http://localhost:8888", // Locally hosted node needed for reasonable indexing speed
+    39750 // First actions relevant to this dapp happen at this block
+)
+
+const actionWatcher = new BaseActionWatcher(
+  actionReader,
+  actionHandler,
+  250, // Poll at twice the block interval for less latency
+)
   
-  // Import Updaters and Effects, which are arrays of objects:
-  // [ { actionType:string, (updater|effect):function }, ... ] 
-  const updaters = require("./updaters")
-  const effects = require("./effects")
-  
-  const actionHandler = new MyActionHandler({
-    updaters,
-    effects,
-  })
-  
-  const actionReader = new NodeosActionReader({
-    nodeosEndpoint: "http://some-nodeos-endpoint:8888", // Locally hosted node needed for reasonable indexing speed
-    startAtBlock: 12345678, // First actions relevant to this dapp happen at this block
-  })
-  
-  const actionWatcher = new BaseActionWatcher({
-    actionReader,
-    actionHandler,
-    pollInterval: 250, // Poll at twice the block interval for less latency
-  })
-  
-  actionWatcher.watch() // Start watch loop
+module.exports = actionWatcher;

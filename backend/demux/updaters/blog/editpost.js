e433d85f549c505581cb2bef9ca234301960826b
TaraTritt <tmt7rc@virginia.edu>
2018-07-24 16:28:35 -0400

    Removing accidental merge conflict errors

--- backend/demux/updaters/blog/editpost.js
+++ backend/demux/updaters/blog/editpost.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-async function editPost(state, payload, blockInfo, context) {
-=======
 async function editPost (state, payload, blockInfo, context) {
->>>>>>> 12a7b56cf73e1d32ec98dd2b583b536b2764c7c6
   try {
     await state.post.findByIdAndUpdate(payload.data._id, payload.data).exec()
   } catch (err) {

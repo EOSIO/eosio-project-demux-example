ac83c393668c23f8bfb97e6a5b78f5a6ff82e233
TaraTritt <tmt7rc@virginia.edu>
2018-07-24 16:12:55 -0400

    Merging

--- backend/demux/updaters/blog/deletepost.js
+++ backend/demux/updaters/blog/deletepost.js
@@ -1,7 +1,9 @@
-function deletePost (state, payload, blockInfo, context) {
-  state.post.findByIdAndDelete(payload.data._id, (err) => {
-    if (err) console.error(err)
-  })
+async function deletePost(state, payload, blockInfo, context) {
+  try {
+    await state.post.findByIdAndDelete(payload.data._id).exec()
+  } catch (err) {
+    console.error(err)
+  }
 }
 
 module.exports = deletePost
--- backend/demux/updaters/blog/likepost.js
+++ backend/demux/updaters/blog/likepost.js
@@ -1,7 +1,9 @@
-function likePost (state, payload, blockInfo, context) {
-  state.post.findByIdAndUpdate(payload.data._id, { $inc: { likes: 1 } }, (err) => {
-    if (err) console.error(err)
-  })
+async function likePost(state, payload, blockInfo, context) {
+  try {
+    await state.post.findByIdAndUpdate(payload.data._id, { $inc: { likes: 1 } }).exec()
+  } catch (err) {
+    console.error(err)
+  }
 }
 
 module.exports = likePost

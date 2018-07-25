b89c6cb4a074b74a1654d024a651500da08e4327
uxlayouts <hellojohnnymac@gmail.com>
2018-07-25 16:18:32 -0400

    move svg inports to css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import registerServiceWorker from './utils/registerServiceWorker'
+import './assets/styles/core.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()

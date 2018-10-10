ed21b6aeecdad6ad11ee3521639bd28e74a60f60
TaraTritt <tmt7rc@virginia.edu>
2018-10-10 12:43:55 -0400

    Renaming api to routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import demux from './services/demux'
-import posts from './api/posts'
+import posts from './routes/posts'
 import io from './utils/io'
 
 let app = express()
